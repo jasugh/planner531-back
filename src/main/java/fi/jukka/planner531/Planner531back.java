e2db951ceab98482cf27679618a49c7b11ce4321
Jukka <joku@jossain.com>
2021-04-05 18:46:37 +0300

    Extra commit

--- src/main/java/fi/jukka/planner531/Planner531back.java
+++ src/main/java/fi/jukka/planner531/Planner531back.java
@@ -2,7 +2,7 @@ package fi.jukka.planner531;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class Planner531back {
 
